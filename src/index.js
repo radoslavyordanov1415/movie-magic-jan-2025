c22e33b8403fa618f3c2227c7149be2927ef9f8b
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-16 15:45:26 +0100

    add and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import handlebars from 'express-handlebars';
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('home', { layout: false });
 });
 
 
