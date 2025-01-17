08ce75d399315ce64799c4409fb17b33bf5469c1
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-17 15:31:57 +0100

    add dynamic details link for each movie

--- src/movies.js
+++ src/movies.js
@@ -1,5 +1,6 @@
 export default [
     {
+        id: 1,
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, thefeisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-ladenission to change the world. Along with her fashionable brother, MacGregor, Dr Houghtonnlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff,o guide them through the serpentine Amazon River in La Quila, his swift wooden boat.ow, as the intrepid trio ventures deeper and deeper into the heart of an impenetrablereen maze, searching for something that cannot be found, a centuries-old curse and theuthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans',
@@ -7,6 +8,7 @@ export default [
     },
 
     {
+        id: 2,
         title: 'The Dark Knight',
         genre: 'Action',
         description: 'The story revolves around Batman, who, after being assassinated by Gothams own Gordon Ramsay, struggles to find his identity and uses his supernatural abilities to protect the city from crime and tyranny.Batman must confront a formidable foe, a cunning clown named Joker, who wants to exact revenge against the criminals who committed the assassination of Gothams finest.',
@@ -14,6 +16,7 @@ export default [
     },
 
     {
+        id: 3,
         title: 'The Matrix',
         genre: 'Sci-Fi',
         description: 'The film follows the world-renowned artificial intelligence, the Matrix, as it navigates a dystopian future in which humanity is trapped in a simulated reality called the Matrix. Agent Smith, a brilliant scientist, becomes entangled in the Matrix and must use his intelligence and programming to escape or be killed.',
