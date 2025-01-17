b1db24a73c20d1c8ced0b0060a207aad57bb0965
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-17 16:20:37 +0100

    parse form data on server

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -8,10 +8,18 @@ movieController.get('/create', (req, res) => {
     res.render('create')
 })
 
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    res.end()
+});
+
+
+
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.findOne(movieId)
-    res.render('details');
+    res.render('details', { movie });
 })
 
 export default movieController
--- src/movies.js
+++ src/movies.js
@@ -4,7 +4,12 @@ export default [
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, thefeisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-ladenission to change the world. Along with her fashionable brother, MacGregor, Dr Houghtonnlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff,o guide them through the serpentine Amazon River in La Quila, his swift wooden boat.ow, as the intrepid trio ventures deeper and deeper into the heart of an impenetrablereen maze, searching for something that cannot be found, a centuries-old curse and theuthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans',
-        imageUrl: './static/img/jungle-cruise.jpeg'
+        imageUrl: '/static/img/jungle-cruise.jpeg',
+        director: 'Jaume Collet-Serra',
+        year: 2021,
+        rating: 6.6,
+        category: 'Movie'
+
     },
 
     {
@@ -12,7 +17,11 @@ export default [
         title: 'The Dark Knight',
         genre: 'Action',
         description: 'The story revolves around Batman, who, after being assassinated by Gothams own Gordon Ramsay, struggles to find his identity and uses his supernatural abilities to protect the city from crime and tyranny.Batman must confront a formidable foe, a cunning clown named Joker, who wants to exact revenge against the criminals who committed the assassination of Gothams finest.',
-        imageUrl: 'https://image.tmdb.org/t/p/original/pKKvCaL1TPTVtbI6EeliyND3api.jpg'
+        imageUrl: 'https://image.tmdb.org/t/p/original/pKKvCaL1TPTVtbI6EeliyND3api.jpg',
+        director: 'Christopher Nolan',
+        year: 2008,
+        rating: 9,
+        category: 'Movie'
     },
 
     {
@@ -20,7 +29,11 @@ export default [
         title: 'The Matrix',
         genre: 'Sci-Fi',
         description: 'The film follows the world-renowned artificial intelligence, the Matrix, as it navigates a dystopian future in which humanity is trapped in a simulated reality called the Matrix. Agent Smith, a brilliant scientist, becomes entangled in the Matrix and must use his intelligence and programming to escape or be killed.',
-        imageUrl: 'https://image.tmdb.org/t/p/original/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg'
+        imageUrl: 'https://image.tmdb.org/t/p/original/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg',
+        director: 'Lana Wachowski, Lilly Wachowski',
+        year: 1999,
+        rating: 8.7,
+        category: 'Movie'
 
     }
 
