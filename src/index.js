e26f2263e7c84ae525dd0328f2c852f8314f0c4f
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-20 02:33:07 +0100

    show movie rating with handlebars helper

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import handlebars from 'express-handlebars';
 import homeController from './controllers/home-controller.js'
 import routes from './routes.js'
 import router from './controllers/home-controller.js';
+import showRatingHelper from './helpers/rating-helper.js';
 
 
 const app = express();
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
+    helpers: {
+        showRating: showRatingHelper,
+    }
 }));
+
+
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
