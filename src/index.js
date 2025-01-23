56e0f260d8f6d45627a5c2568d94e942b23d0d9c
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-23 13:22:46 +0100

    setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import homeController from './controllers/home-controller.js'
 import routes from './routes.js'
-import router from './controllers/home-controller.js';
 import showRatingHelper from './helpers/rating-helper.js';
-
+import mongoose from 'mongoose';
 
 const app = express();
 
+// Db configuration
+try {
+    const uri = 'mongodb://localhost:27017/magic-movies-jan2025'
+    await mongoose.connect(uri);
+
+    console.log('Connected to the database...');
+} catch (err) {
+    console.log('Error connecting to the database...');
+    console.error(err);
+}
+
+
+// Handlebars configuration
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     helpers: {
@@ -19,15 +31,17 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+
+// Express configuration
 app.use('/static', express.static('src/public'))
 app.use(express.urlencoded({ extended: false })); // Learn express to parse form data 
 
 app.use(homeController);
 
-
+// Setup routes
 app.use(routes);
 
 
-
+// Start the server
 app.listen(5000, () => console.log('Server listening on port http://localhost:5000...'));
 
