19b58681a7926de15f56c065de664152e3cb3601
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-24 13:57:15 +0100

    show cast in attach page

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -4,6 +4,13 @@ import Cast from "../models/Cast.js";
 
 
 export default {
+
+
+    getAll() {
+        return Cast.find({})
+    },
+
+
     create(castData) {
         return Cast.create(castData)
     }
