72ede1301b3ad43bf4d20ff137970c1d02153ad9
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-01-24 14:45:42 +0100

    add many to many movie relationship

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Types } from 'mongoose';
 
 // Create schema 
 const movieSchema = new Schema({
@@ -10,8 +10,12 @@ const movieSchema = new Schema({
     imageUrl: String,
     rating: Number,
     description: String,
+    casts: [{
+        type: Types.ObjectId,
+        ref: 'Cast'
+    }],
 
-})
+});
 
 // Create model
 const Movie = model('Movie', movieSchema);
