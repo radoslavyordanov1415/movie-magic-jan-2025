7923c2abd3f68ae08e51201389bccfcd3dd99d46
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-02 18:26:36 +0100

    send cookie token to client on login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -22,16 +22,15 @@ authController.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-
         const token = await authService.login(email, password);
-        console.log(token);
 
+
+        res.cookie('auth', token);
+        res.redirect('/');
     } catch (err) {
         console.log(err.message)
-        return res.redirect('/404')
+        res.redirect('/404')
     };
-
-    res.redirect('/');
 });
 
 export default authController;
