8ced42326e58aa097af88bb584c387d09231c3af
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-02 18:17:01 +0100

    add login functionality with jwt token

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,8 +1,33 @@
 import User from "../models/User.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const SECRET = '$2b$10$12Z36xeLml37nkA352FK9.GG2PZ9D8UpDdg24qm.ll1vv000ms.7K'
 export default {
     register(userData) {
         return User.create(userData);
 
+    },
+
+    async login(email, password) {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new Error('Invalid email or password');
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            throw new Error('Invalid email or password');
+        };
+        const payload = {
+            id: user.id,
+            email: user.email
+        };
+
+
+        const token = jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
+
+        return token;
     }
 };
