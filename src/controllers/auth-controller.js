5fc00284256e9d6cbdad9d74c57180cad95a8684
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-02 17:08:05 +0100

    send data to register end point

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -5,6 +5,12 @@ authController.get('/register', (req, res) => {
     res.render('auth/register',);
 });
 
+authController.post('/register', async (req, res) => {
+    const userData = req.body;
+    console.log(userData);
 
+    res.end()
+
+})
 
 export default authController;
