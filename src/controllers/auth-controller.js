f31d1d206c039b50d3e81607ab212e67c7d192f5
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-02 17:04:41 +0100

    add register page

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
 
 const authController = Router();
+authController.get('/register', (req, res) => {
+    res.render('auth/register',);
+});
+
+
 
 export default authController;
