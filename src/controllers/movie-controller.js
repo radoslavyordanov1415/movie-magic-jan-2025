3c786380dcda805fcbfd770d0d211a83c176be63
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-03 13:18:17 +0100

    show dynamic select value

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -69,9 +69,28 @@ movieController.get('/:movieId/delete', async (req, res) => {
 movieController.get('/:movieId/edit', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId);
-    res.render('movie/edit', { movie })
+    const categories = getCategoriesViewData(movie.category);
+    res.render('movie/edit', { movie, categories });
 
 });
+
+function getCategoriesViewData(category) {
+    let categoriesMap = {
+
+        'tv-show': 'TV Show',
+        'animation': 'Animation',
+        'movie': 'Movie',
+        'documentary': 'Documentary',
+        'short-film': 'Short Film'
+    };
+    const categories = Object.keys(categoriesMap).map(value => ({
+        value,
+        label: categoriesMap[value],
+        selected: value === category ? 'selected' : ''
+    }));
+
+    return categories;
+};
 
 
 
