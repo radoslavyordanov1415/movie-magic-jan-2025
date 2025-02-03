7208636b92dd23400c97eb54b895858ee6251cc0
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-03 12:33:11 +0100

    show data in edit form

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -66,8 +66,10 @@ movieController.get('/:movieId/delete', async (req, res) => {
     res.redirect('/')
 });
 
-movieController.get('/:movieId/edit', (req, res) => {
-    res.render('movie/edit')
+movieController.get('/:movieId/edit', async (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = await movieService.getOne(movieId);
+    res.render('movie/edit', { movie })
 
 });
 
