fe5e25f9e146d403b0798b90c8537054caa8bca0
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-03 11:58:38 +0100

    add logout functionality

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -33,4 +33,9 @@ authController.post('/login', async (req, res) => {
     };
 });
 
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
+
 export default authController;
