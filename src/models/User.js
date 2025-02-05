3ff23a6120edc57ec49d5df07098b9cb74756105
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-02-05 13:44:04 +0100

    add user validations

--- src/models/User.js
+++ src/models/User.js
@@ -3,18 +3,37 @@ import bcrypt from "bcrypt";
 
 
 const userSchema = new Schema({
-    email: String,
-    password: String,
-
+    email: {
+        type: String,
+        required: true,
+        unique: true, //*This is not validator, its index
+        match: /\@[a-zA-Z]+.[a-zA-Z]+$/,
+        minLength: 10,
+    },
+    password: {
+        type: String,
+        required: true,
+        match: /^\w+$/,
+        minLength: 6,
+    },
 });
 
+
+userSchema.virtual('rePassword')
+    .set(function (rePassword) {
+        if (rePassword !== this.password) {
+            throw new Error('Passwords do not match');
+        }
+    });
 
 userSchema.pre('save', async function () {
     this.password = await bcrypt.hash(this.password, 10);
 
-
-})
+});
 
 const User = model("User", userSchema);
+
+
+
 
 export default User;
